49ec426aa3e28412abecac91383b30c897adfea6
unknown <santiagoyuri373@gmail.com>
2020-10-07 16:27:29 -0300

    Atividade crud farmacia Parte II

--- farmacia/farmacia/src/main/java/org/generation/farmacia/model/Categoria.java
+++ farmacia/farmacia/src/main/java/org/generation/farmacia/model/Categoria.java
@@ -1,12 +1,19 @@
 package org.generation.farmacia.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "categoria")
@@ -20,6 +27,18 @@ public class Categoria {
 	private String nome;
 	
 	private boolean prescricao_medica;
+	
+	@OneToMany(mappedBy = "categoria", cascade= CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
 
 	public long getId() {
 		return id;
---
+++ farmacia/farmacia/src/main/java/org/generation/farmacia/model/Produto.java
@@ -0,0 +1,77 @@
+package org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_produto")
+public class Produto {
+	@Id
+	@GeneratedValue (strategy=GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 2, max=200)
+	private String nome;
+	
+	
+	private double preco;
+	
+	private long quantidade_estoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public long getQuantidade_estoque() {
+		return quantidade_estoque;
+	}
+
+	public void setQuantidade_estoque(long quantidade_estoque) {
+		this.quantidade_estoque = quantidade_estoque;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
